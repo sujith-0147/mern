e69803967e5791706a343db6b96cf332327a3931
gunachilukuri <136338624+gunachilukuri@users.noreply.github.com>
2025-06-24 19:37:06 +0530

    schema

--- src/app.js
+++ src/app.js
@@ -3,19 +3,58 @@ const adminauth = require("./middlewares/auth");
 const connectdB=require("./config/database");
 const User=require("./models/user");
 const app = express();
+app.use(express.json());
+ 
 app.post("/signup",async (req,res)=>{
-     
-const userobj={
-    firstname:"Rama",
-    lastname:"sujith",
-    emailid:"dudduramasujith7@gmail.com",
-    password:"Ramasujith1.",
-    gender:"male"
-}
- const user=new User(userobj);
+    //  console.log(req.body);
+
+ const user=new User(req.body);
  await user.save();
  res.send("sucessfully completed");
 });
+
+
+app.get("/user",async (req,res)=>{
+
+   const user= await User.find({email:req.body.email});
+   if(user.length===0){
+    res.send("Not found");
+   }
+   else{
+   res.send(user);
+   }
+
+});
+app.get("/feed",async (req,res)=>{
+
+   const user= await User.find({});
+   if(user.length===0){
+    res.send("Not found");
+   }
+   else{
+   res.send(user);
+   }
+
+});
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId;
+   const user= await User.findByIdAndDelete(userId);
+res.send("user id deleteted");
+});
+app.patch("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    const user=await User.findByIdAndUpdate(userId,{email:"ramasujith7@gmail.com"},{
+     runValidators:true   
+    });
+    res.send("user updated sucessfully");
+
+
+});
+
+
+
+
+
 connectdB()
     .then(() => {
         console.log("Database connected successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,57 @@
-const mongoose =require("mongoose");
-const userschema= new mongoose.Schema({
-    firstname:{
-        type:String
-    }  ,
-    lastname:{
-        type:String
-    },
-    email:{
-        type:String
-    },
-    password:{
-        type:String
-    },
-    age:{
-        type:Number
-    },
-    gender:{
-        type:String
-    }
+const mongoose = require("mongoose");
 
- });
-  const User=mongoose.model("User",userschema );
-  module.exports=User;
+const userschema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        minLength:4,
+        maxLength:100,
+    },
+    lastname: {
+        type: String
+    },
+    email: {
+        type: String,
+        lowercase:true,
+        required: true,
+        unique:true,
+        trim:true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        min:18,
+
+    },
+    gender: {
+        type: String,
+        validate(value){
+
+            if(!["male","female","others"].includes(value) ){
+                throw new Error("grnder not valid");
+                
+            }
+        },
+        
+    },
+    photoUrl:{
+        type:String,
+        default:"https://freepngimg.com/thumb/hinduism/30434-5-hanuman-hd.png",
+    },
+    about:{
+        type:String,
+        default:"I am looking good"
+
+    },
+    skills:{
+        type:[String]
+    }
+},{
+    timestamps:true,
+});
+
+const User = mongoose.model("User", userschema);
+module.exports = User;
